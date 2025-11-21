aed08ddfd9281088f61b1cfda1ba8590ce8ef007
mvela31 <mv31@illinois.edu>
2025-11-21 08:13:56 +0100

    changes

---
+++ part3b.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+## Functions for Group B
+
+## Functions for group C
+def extract_peaks(main_weld_signal):
+    """
+     Input: Main weld signal
+     Output : first 2 peaks magnitude and frequency
+    """
+
+    
